0ea56210cc0678b654e20ad41fced2042778c47f
dokkalemon <dottori.roberto@gmail..com>
2021-11-29 15:15:24 +0100

    add Main and Card componentsx

---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+      <li>Titolo:</li>
+      <li>Titolo Originale</li>
+      <li>Lingua</li>
+      <li>Voto</li>
+  </ul>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+
+      <Card />
+
+  </main>
+</template>
+
+<script>
+import Card from '@/components/Card.vue'
+
+export default {
+    name: 'Main',
+    components: {
+        Card
+    }
+}
+</script>
+
+<style scoped lang="scss">
+main {
+    width: 100%;
+}
+
+</style>
