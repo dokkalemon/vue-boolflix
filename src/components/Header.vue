2d0199c3870fd8f1695b8746623f814521f59109
dokkalemon <dottori.roberto@gmail..com>
2021-11-29 15:10:03 +0100

    add Header

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <form action="">
+        <input type="text" placeholder="Inserisci il titolo di un film">
+        <button type="submit">Search</button>
+
+
+    </form>
+  
+
+
+
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style>
+
+</style>
